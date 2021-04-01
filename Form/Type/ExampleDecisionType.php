9745ca05aa03094afb0c9daedd1f09bae93fb917
Daniel Hilst Selli <daniel@versatushpc.com.br>
2021-04-01 18:53:48 -0300

    Add FormType for using as input for decision

---
+++ Form/Type/ExampleDecisionType.php
@@ -0,0 +1,28 @@
+<?php
+namespace MauticPlugin\HelloWorldBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+use Mautic\CoreBundle\Form\Type\SortableListType;
+use Mautic\CampaignBundle\Form\Type\CampaignListType;
+use MauticPlugin\NisiBundle\Utils\Utils;
+
+class ExampleDecisionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add("checkpoint", NumberType::class, [
+            "label"      => "plugin.nisi.checkpoint",
+            "attr"       => ["class" => "form-control"],
+            "label_attr" => ["class" => "control-label"],
+        ]);
+    }
+}
